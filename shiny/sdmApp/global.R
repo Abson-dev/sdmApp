8c9ed9716923c3c32c47f278b03de5809ba7bcfa
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2020-11-29 22:25:41 +0000

    Update global.R

--- shiny/sdmApp/global.R
+++ shiny/sdmApp/global.R
@@ -5,6 +5,29 @@ library(rhandsontable)
 library(haven)
 library(shinyBS)
 library(data.table)
+library(shiny)
+library(sf) # classes and functions for vector data
+library(raster)# classes and functions for raster data
+library(ggplot2)
+library(biomod2)
+library(grid)
+library(rhandsontable)
+library(haven)
+library(DT)
+library(shinyBS)
+library(data.table)
+library(readxl)
+library(shinyFiles)
+library(shinydashboard)
+library(SSDM)
+library(automap)
+library(blockCV)
+library(tidyverse)
+library(ggpubr)
+library(CENFA)
+library(dismo)
+library(randomForest)
+library(kernlab)
 
 if (!getShinyOption("sdmAppInvoked", FALSE)) {### Beginning required code for deployment
   .startdir <- .guitheme <- .guijsfile <- NULL
@@ -17,3 +40,7 @@ if (!getShinyOption("sdmAppInvoked", FALSE)) {### Beginning required code for de
   shinyOptions(.guitheme = "ihsn-root.css")
   shinyOptions(.guijsfile = "js/ihsn-style.js")
 }## End of deployment code
+
+
+
+
