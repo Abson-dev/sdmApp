6a3a47b07bc8273a972fdf19e4b1ed9a3bc0f098
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2020-12-04 10:50:22 +0000

    Update ui_preparation.R

--- inst/shiny/sdmApp/controllers/ui_preparation.R
+++ inst/shiny/sdmApp/controllers/ui_preparation.R
@@ -418,7 +418,7 @@ output$ui_spatial_blocks<-renderUI({
   })
   output$test_train_plot<-renderPlot({
     spatialblock<-spatialblock()
-    Explorer(spatialblock, data$Env, pa_data(),load.occ$fold)
+    sdmApp::sdmApp_fold_Explorer(spatialblock, data$Env, pa_data(),load.occ$fold)
   })
 
   fluidRow(column(12, h4("Spatial blocking"), align="center"),
