67a94658f75915d1e8d8abbab552a764bc8f53c3
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2020-12-04 08:09:16 +0000

    Update ui_code.R

--- inst/shiny/sdmApp/controllers/ui_code.R
+++ inst/shiny/sdmApp/controllers/ui_code.R
@@ -7,8 +7,17 @@ output$current_code <- renderText({
   paste0("<pre class='r'><code class='r' id='codeout'>",paste(highr:::hi_html(current_code()), collapse="\n"),"</code></pre>")
 })
 
+output$ui_code_nodata <- renderUI({
+  return(list(
+    noInputData(uri="ui_code"),
+    fluidRow(column(12, tags$br(), p(""), align="center"))
+  ))
+})
 # GUI-output to view script
 output$ui_code <- renderUI({
+  if(length(load.occ$Pcol) == 0){
+    return(uiOutput("ui_code_nodata"))}
+  else{
   out <- fluidRow(
     column(12, h3("View the current generated script"), class="wb-header"),
     column(12, p("Browse and download the script used to generate your results. These can be used later as a reminder of what you did or entered into R from command-line to reproduce results."), class="wb-header-hint"),
@@ -16,9 +25,6 @@ output$ui_code <- renderUI({
     column(12, tags$br(), uiOutput("current_code"))
   )
 
-  # if (!is.null(obj$lastscriptexport)) {
-  #   out <- list(out, fluidRow(
-  #     column(12, tags$br(), p("Information: the last script you have exported was saved as", code(obj$lastscriptexport)), align="center")))
-  # }
   out
+  }
 })
