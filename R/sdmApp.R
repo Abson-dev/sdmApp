04099cc29be3ad00279e31ddf6754b49eda9bf5e
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2020-12-04 22:50:22 +0000

    Update sdmApp.R

--- R/sdmApp.R
+++ R/sdmApp.R
@@ -49,7 +49,7 @@ sdmApp<-function (maxRequestSize = 50, debug = FALSE, theme = "IHSN",
           ..., shiny.server = FALSE)
 {
   if (!shiny.server)
-    runApp(sdmApp(maxRequestSize, debug, theme, ..., shiny.server = TRUE))
+    shiny::runApp(sdmApp(maxRequestSize, debug, theme, ..., shiny.server = TRUE))
   if (!is.numeric(maxRequestSize)) {
     stop("argument 'maxRequestSize' must be numeric!\n")
   }
@@ -66,35 +66,35 @@ sdmApp<-function (maxRequestSize = 50, debug = FALSE, theme = "IHSN",
             1024^2)
   options(shiny.fullstacktrace = debug)
   options(shiny.trace = debug)
-  shinyOptions(.startdir = getwd())
-  shinyOptions(.appDir = appDir)
+  shiny::shinyOptions(.startdir = getwd())
+  shiny::shinyOptions(.appDir = appDir)
   if (!theme %in% c("yeti", "journal", "flatly",
                     "IHSN")) {
     stop("Invalid value for argument 'theme'\n")
   }
   if (theme == "yeti") {
-    shinyOptions(.guitheme = "bootswatch_yeti.css")
-    shinyOptions(.guijsfile = NULL)
+    shiny::shinyOptions(.guitheme = "bootswatch_yeti.css")
+    shiny::shinyOptions(.guijsfile = NULL)
   }
   if (theme == "journal") {
-    shinyOptions(.guitheme = "bootswatch_journal.css")
-    shinyOptions(.guijsfile = NULL)
+    shiny::shinyOptions(.guitheme = "bootswatch_journal.css")
+    shiny::shinyOptions(.guijsfile = NULL)
   }
   if (theme == "flatly") {
-    shinyOptions(.guitheme = "bootswatch_flatly.css")
-    shinyOptions(.guijsfile = NULL)
+    shiny::shinyOptions(.guitheme = "bootswatch_flatly.css")
+    shiny::shinyOptions(.guijsfile = NULL)
   }
   if (theme == "IHSN") {
-    shinyOptions(.guitheme = "ihsn-root.css")
-    #shinyOptions(.guijsfile = "js/ihsn-style.js")
+    shiny::shinyOptions(.guitheme = "ihsn-root.css")
+    #shiny::shinyOptions(.guijsfile = "js/ihsn-style.js")
   }
   source_from_appdir <- function(filename) {
     source(file.path(appDir, filename), local = parent.frame(),
            chdir = TRUE)$value
   }
-  shinyOptions(sdcAppInvoked = TRUE)
+  shiny::shinyOptions(sdcAppInvoked = TRUE)
   source_from_appdir("global.R")
-  shinyOptions(sdcAppInvoked = NULL)
+  shiny::shinyOptions(sdcAppInvoked = NULL)
   shiny::shinyApp(ui = source_from_appdir("ui.R"), server = source_from_appdir("server.R"),
                   options = list(launch.browser = TRUE, ...))
 }
