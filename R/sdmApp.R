9f9307aa2d0c612f42fb5c34561078870a2d6855
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2020-12-05 13:35:06 +0000

    Update sdmApp.R

--- R/sdmApp.R
+++ R/sdmApp.R
@@ -41,9 +41,9 @@
 #' @import kernlab
 #'
 #' @examples
-#' \dontrun{
+#' #load the package
+#' library(sdmApp)
 #' sdmApp()
-#' }
 sdmApp<-function (maxRequestSize = 50, debug = FALSE, theme = "IHSN",
           ..., shiny.server = FALSE)
 {
