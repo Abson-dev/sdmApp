abe285e8fc4dc775fb23a22edfc56afba218f527
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2020-12-07 23:17:34 +0000

    Update server.R

--- inst/shiny/sdmApp/server.R
+++ inst/shiny/sdmApp/server.R
@@ -326,8 +326,16 @@ shinyServer(function(session, input, output) {
         load.occ$df_occ<-as.data.frame(read_excel(file))
       }
       else if (input$file_type == "SPSS") {
-        load.occ$columns <- names(as.data.frame(read_sav(file)))
-        load.occ$df_occ<-as.data.frame(read_sav(file))
+        a = try(withProgress(message = 'Loading SPSS occurence data...',
+                             as.data.frame(read_sav(file))))
+        if(inherits(a, 'try-error')){
+          output$Envbug <- renderUI(p('SPSS occurence data loading failed, please check your inputs and try again'))
+        }
+        else{
+          output$Envbug <- renderUI(p())
+          load.occ$columns <- names(a)
+          load.occ$df_occ<-a
+          }
       }
       else if (input$file_type == "Stata") {
         load.occ$columns <- names(as.data.frame(read_dta(file)))
