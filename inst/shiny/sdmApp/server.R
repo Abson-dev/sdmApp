d3511ee0ac4923ec5c29f96eebe37e68c4c90573
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2020-12-07 21:31:02 +0000

    Update server.R

--- inst/shiny/sdmApp/server.R
+++ inst/shiny/sdmApp/server.R
@@ -323,7 +323,8 @@ shinyServer(function(session, input, output) {
       }
       else if (input$file_type == "Excel") {
         load.occ$columns <- names(read_excel(file))
-        load.occ$df_occ<-read_excel(file)
+        load.occ$df_occ<-as.data.frame(read_excel(file,
+                                                  stringsAsFactors = FALSE))
       }
       else if (input$file_type == "SPSS") {
         load.occ$columns <- names(read_sav(file))
