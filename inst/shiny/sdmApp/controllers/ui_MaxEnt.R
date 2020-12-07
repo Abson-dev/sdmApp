d18b9ed83628d02e2a028b48a08a45f75959e71b
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2020-12-07 22:58:52 +0000

    Update ui_MaxEnt.R

--- inst/shiny/sdmApp/controllers/ui_MaxEnt.R
+++ inst/shiny/sdmApp/controllers/ui_MaxEnt.R
@@ -1,36 +1,36 @@
 ##### Maxent content
 output$ui_MaxEnt<-renderUI({
 
-  Specdata<-reactive({
-    dsf<-load.occ$select
-    dsf<-dsf %>% dplyr::rename(lon=load.occ$lon,lat=load.occ$lat)
-    dsf
-  })
-
-  Specdata_Presence<-reactive({
-    dsf<-Specdata()
-    dsf<-dsf[dsf[,ncol(dsf)] == 1,]
-    sp::coordinates(dsf) <-~lon+lat
-    sp::proj4string(dsf) <-raster::crs(data$Env)
-    dsf
-  })
-
-  glc<-reactive({
-    GLcenfa(x = data$Env)
-  })
-
-  mod.enfa<-reactive({
-    pr<-Specdata_Presence()
-    pr@data$load.occ$spec_select<-as.numeric(pr@data$load.occ$spec_select)
-    CENFA::enfa(x = data$Env, s.dat = pr, field = load.occ$spec_select)
-  })
-  enfa_plot<-reactive({
-    glc <- glc()
-
-    mod.enfa <- mod.enfa()
-    CENFA::scatter(x = mod.enfa, y = glc,n=nlayers(data$Env),p=1)
-  })
-  output$enfa_var<-renderPlot({
+  # Specdata<-reactive({
+  #   dsf<-load.occ$select
+  #   dsf<-dsf %>% dplyr::rename(lon=load.occ$lon,lat=load.occ$lat)
+  #   dsf
+  # })
+  #
+  # Specdata_Presence<-reactive({
+  #   dsf<-Specdata()
+  #   dsf<-dsf[dsf[,ncol(dsf)] == 1,]
+  #   sp::coordinates(dsf) <-~lon+lat
+  #   sp::proj4string(dsf) <-raster::crs(data$Env)
+  #   dsf
+  # })
+  #
+  # glc<-reactive({
+  #   GLcenfa(x = data$Env)
+  # })
+  #
+  # mod.enfa<-reactive({
+  #   pr<-Specdata_Presence()
+  #   pr@data$load.occ$spec_select<-as.numeric(pr@data$load.occ$spec_select)
+  #   CENFA::enfa(x = data$Env, s.dat = pr, field = load.occ$spec_select)
+  # })
+  # enfa_plot<-reactive({
+  #   glc <- glc()
+  #
+  #   mod.enfa <- mod.enfa()
+  #   CENFA::scatter(x = mod.enfa, y = glc,n=nlayers(data$Env),p=1)
+  # })
+  output$enfa_var_Maxent<-renderPlot({
     enfa_plot()
   })
 
@@ -141,7 +141,7 @@ output$ui_MaxEnt<-renderUI({
                                                tabPanel("Specie predictors",
                                                         selectInput('var_expl', 'Please select the specie predictors', names(data$Env), multiple = TRUE, selectize = TRUE),
                                                         myActionButton("MaxEnt",label=("Apply MaxEnt"), "primary"),
-                                                        plotOutput("enfa_var")
+                                                        plotOutput("enfa_var_Maxent")
                                                ),
                                                tabPanel("Map",
 
