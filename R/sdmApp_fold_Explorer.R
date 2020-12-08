3c3d2fb099b3352b935a08e6ac2ec24bb79f19c4
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2020-12-08 03:07:42 +0000

    Update sdmApp_fold_Explorer.R

--- R/sdmApp_fold_Explorer.R
+++ R/sdmApp_fold_Explorer.R
@@ -24,7 +24,6 @@
 #' @description Blocks‐to‐folds is one of the key steps for species modelling because species data are rarely evenly dispersed over landscapes. When random selection of folds is chosen, constraints can be set to avoid folds with little or no presence or (where relevant) absence data. Techniques are also implemented for finding block‐to‐fold allocations that achieve most even spread of species data across folds (e.g., a similar number of presence and absence records in each fold). In systematic allocation, blocks are numbered and assigned to folds sequentially. The number of folds can be specified by the user in the systematic and random allocations and it can be equal or less than the number of blockWe note that in all the spatial blocking scenarios, all data in the test folds (including background points, if relevant) are excluded from the training datasetss.
 #'
 #' @examples
-#' \dontrun{
 #' # load blockCV package data
 #' library(blockCV)
 #' awt <- raster::brick(system.file("extdata", "awt.grd", package = "blockCV"))
@@ -37,7 +36,6 @@
 #' rasterLayer = awt,theRange = 70000,k = 5,
 #' selection = "random",iteration = 100)
 #' sdmApp_fold_Explorer(sb,awt,pa_data,1)
-#' }
 sdmApp_fold_Explorer<-function (blocks, rasterLayer, speciesData, num) {
   if (is.null(rasterLayer)) {
     stop("A raster layer should be provided")
