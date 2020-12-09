d708cf7e2fa4c9c06bad88dce42db07394463a3e
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2020-12-09 16:20:09 +0000

    Update ui_mahal.R

--- inst/shiny/sdmApp/controllers/ui_mahal.R
+++ inst/shiny/sdmApp/controllers/ui_mahal.R
@@ -163,6 +163,8 @@ output$ui_mahal<-renderUI({
                                                ),
                                                tabPanel("Variable response",
                                                         selectInput('response_var_Mahal', 'Please select the variable to get its ecological response', names(data$enfa), multiple = FALSE, selectize = TRUE),
+                                                        radioButtons(inputId = "plot_type_response_var_Mahal", label = "Select the file type to export", choices = list("png", "pdf"),inline = TRUE),
+                                                        downloadButton('download_response_var_Mahal', 'Download'),
                                                         plotOutput("response_eco_Mahal")
                                                )
                                                # ,
