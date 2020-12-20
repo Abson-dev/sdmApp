54d8589e27c753588faa7503f6e2a45c330d9207
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2020-12-20 13:04:35 +0000

    Update sdmApp_TimesRasters.R

--- R/sdmApp_TimesRasters.R
+++ R/sdmApp_TimesRasters.R
@@ -7,12 +7,12 @@
 #' @export
 #'
 #' @examples
-#' \donttest{
 #' r <- raster::raster(system.file("extdata","AETI.tif",package = "sdmApp"))
-#' r2 <- r > 4000
+#' r2 <- r > raster::cellStats(r, stat='mean', na.rm=TRUE)
+#' r <- r/raster::maxValue(r)
+#' names(r) <- "propability of occurence"
 #' z<-sdmApp_TimesRasters(r,r2)
 #' sdmApp_RasterPlot(z)
-#' }
 sdmApp_TimesRasters<-function(x,y){
   z<-x * y
   names(z)<-names(x)
