3c310ecfcae7a8acd8426c41ae987624948255c8
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2020-12-21 02:12:22 +0000

    Update server.R

--- inst/shiny/sdmApp/server.R
+++ inst/shiny/sdmApp/server.R
@@ -452,86 +452,24 @@ shinyServer(function(session, input, output) {
     load.occ$sac
   })
 
+  Cor_plotInput <- function(){
+    p1 =   ggcorrplot::ggcorrplot(mat(),ggtheme = ggplot2::theme_gray,
+                                  hc.order = TRUE,
+                                  type = "lower",
+                                  p.mat = p.mat(),
+                                  colors = c("#6D9EC1", "white", "#E46726"))
+
+    return(p1)
+  }
 
 
+  enfa_plotInput <- function(){
+    glc <- glc()
 
+    mod.enfa <- mod.enfa()
+    p1=CENFA::scatter(x = mod.enfa, y = glc,n=nlayers(data$Env),p=1)
+    return(p1)
+  }
 
-  output$download_cor_mat <- downloadHandler(
-    filename = function() {
-      paste('correlation_matrix', Sys.Date(), '.csv', sep='')
-    },
-    content = function(file) {
-      utils::write.csv(mat(), file)
-    }
-  )
-
-  output$download_Marg_Spec <- downloadHandler(
-    filename = function() {
-      paste('Marg_Spec', Sys.Date(), '.csv', sep='')
-    },
-    content = function(file) {
-      utils::write.csv(marg_spec(), file)
-    }
-  )
-
-  ## a plot function
-  Cor_plotInput <- reactive({
-    ggcorrplot::ggcorrplot(mat(),ggtheme = ggplot2::theme_gray,
-                           hc.order = TRUE,
-                           type = "lower",
-                           p.mat = p.mat(),
-                           colors = c("#6D9EC1", "white", "#E46726"))
-  })
-  output$download_cor_plot <- downloadHandler(
-    filename =  function() {
-      paste("cor_plot", input$plot_type_cor, sep=".")
-    },
-    # content is a function with argument file. content writes the plot to the device
-    content = function(file) {
-      if(input$plot_type_cor == "png"){
-        grDevices::png(file) # open the pdf device
-        Cor_plotInput()
-        dev.off()  # turn the device off
-      }
-
-      else{
-        grDevices::pdf(file) # open the pdf device
-        Cor_plotInput()
-        dev.off()  # turn the device off
-      }
-    }
-    )
-
-
-  output$download_Bioclim <- downloadHandler(
-    filename =  function() {
-      paste("Bioclim_N_Blocking", input$plot_type_Bioclim, sep=".")
-    },
-    # content is a function with argument file. content writes the plot to the device
-    content = function(file) {
-      if(input$plot_type_Bioclim == "png"){
-        # grDevices::png(file) # open the pdf device
-        # Cor_plotInput()
-        # dev.off()  # turn the device off
-      }
-
-      if(input$plot_type_Bioclim == "png"){
-        # grDevices::png(file) # open the pdf device
-        # Cor_plotInput()
-        # dev.off()  # turn the device off
-      }
-
-      if(input$plot_type_Bioclim == "tif"){
-        #r <- raster(system.file("external/test.grd", package="raster"))
-        res <- writeRaster(load.occ$Bioclim, filename=file, format="GTiff", overwrite=TRUE)
-
-        # Show the corresponding output filename
-        print(res@file@name)
-
-        # Rename it to the correct filename
-        file.rename(res@file@name, file)
-      }
-    }
-  )
 
 })
