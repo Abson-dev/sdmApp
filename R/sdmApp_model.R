e53d5f275caa6f6b7beae908eab6910949738e6e
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2021-01-28 12:57:50 +0000

    Update sdmApp_model.R

--- R/sdmApp_model.R
+++ R/sdmApp_model.R
@@ -1,16 +1,16 @@
 
 #' Title
 #'
-#' @param type "B" or "NB"
-#' @param model c("Bioclim","Domain","Mahal","GLM","MaxEnt","RF","SVM")
-#' @param nfold number of fold
-#' @param occ species data
-#' @param pred specie predictor
+#' @param type logical. If set to true, allows the function to model with spatial blocking strategy.
+#' @param algorithm character. Choice of the algorithm to be run (see details below).
+#' @param nfold numeric.Number of fold
+#' @param occ data frame. Occurrence table
+#' @param pred raster object. Raster object of environmental variable
 #'
 #' @return
 #' @export
 #'
 #' @examples
-sdmApp_model<-function(type,model,nfold,occ,pred){
+sdmApp_model<-function(type,algorithm,nfold,occ,pred){
 
 }
