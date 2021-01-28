19967d80957526d2231d9c7a78e01d5b587faed6
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2021-01-28 01:10:58 +0000

    Create sdmApp_model.R

---
+++ R/sdmApp_model.R
@@ -0,0 +1,16 @@
+
+#' Title
+#'
+#' @param type "B" or "NB"
+#' @param model c("Bioclim","Domain","Mahal","GLM","MaxEnt","RF","SVM")
+#' @param nfold number of fold
+#' @param occ species data
+#' @param pred specie predictor
+#'
+#' @return
+#' @export
+#'
+#' @examples
+sdmApp_model<-function(type,model,nfold,occ,pred){
+
+}
