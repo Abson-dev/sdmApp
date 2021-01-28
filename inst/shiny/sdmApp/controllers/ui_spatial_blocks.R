ae9636f4edd048cd40e7feb6ac2618d94e8288f2
Aboubacar HEMA <54441886+Abson-dev@users.noreply.github.com>
2021-01-28 01:10:52 +0000

    Update ui_spatial_blocks.R

--- inst/shiny/sdmApp/controllers/ui_spatial_blocks.R
+++ inst/shiny/sdmApp/controllers/ui_spatial_blocks.R
@@ -1,48 +1,53 @@
-
+# range_sp<-reactive({
+#   sac<-load.occ$sac
+#   round(sac$range,0)
+# })
 
 
 output$ui_spatial_blocks<-renderUI({
-  observeEvent(input$sp_block,{
-  observeEvent(input$number_fold,{
-    load.occ$k<-input$number_fold
-  })
+  observeEvent(input$block_button,{
+   observeEvent(input$number_fold,{
+     load.occ$k<-input$number_fold
+   })
 
-  observeEvent(input$allocation_fold,{
+   observeEvent(input$allocation_fold,{
 
-    load.occ$allocation_fold<-input$allocation_fold
-  })
-  sp_Specdata<-reactive({
-    dsf<-load.occ$select
-    dsf[,load.occ$spec_select]<-as.factor(dsf[,load.occ$spec_select])
-    dsf<-dsf %>% dplyr::rename(lon=load.occ$lon,lat=load.occ$lat)
-    dsf
-  })
+     load.occ$allocation_fold<-input$allocation_fold
+   })
+   sp_Specdata<-reactive({
+     dsf<-load.occ$select
+     dsf[,load.occ$spec_select]<-as.factor(dsf[,load.occ$spec_select])
+     dsf<-dsf %>% dplyr::rename(lon=load.occ$lon,lat=load.occ$lat)
+     dsf
+   })
 
-  sp_pa_data<-reactive({
-    load.occ$sp_pa_data<-sf::st_as_sf(sp_Specdata(), coords = c("lon","lat"), crs = crs(data$Env))
-    load.occ$sp_pa_data
-  })
-  spatialblock<-reactive({
-    a = try(withProgress(message = 'Spatial blocking',
-                         blockCV::spatialBlock(speciesData = sp_pa_data(),
-                                      species = load.occ$spec_select,
-                                      rasterLayer = data$var_auto,
-                                      theRange = range(), #load.occ$range, # size of the blocks
-                                      k = load.occ$k,
-                                      showBlocks = TRUE,
-                                      selection = load.occ$allocation_fold,
-                                      iteration = 100, # find evenly dispersed folds
-                                      biomod2Format = FALSE,
-                                      xOffset = 0, # shift the blocks horizontally
-                                      yOffset = 0)))
-    if(inherits(a, 'try-error'))
-      {
-      output$Envbug_sp <- renderUI(p('Spatial blocking failed, please check your inputs and try again!'))
-    } else {
-      output$Envbug_sp <- renderUI(p())
-      a
-    }
-  })
+   sp_pa_data<-reactive({
+     load.occ$sp_pa_data<-sf::st_as_sf(sp_Specdata(), coords = c("lon","lat"), crs = crs(data$Env))
+     load.occ$sp_pa_data
+   })
+
+   spatialblock<-reactive({
+     a = try(withProgress(message = 'Spatial blocking',
+                          blockCV::spatialBlock(speciesData = sp_pa_data(),
+                                                species = load.occ$spec_select,
+                                                rasterLayer = data$var_auto,
+                                                theRange = range(), #load.occ$range, # size of the blocks
+                                                k = load.occ$k,
+                                                showBlocks = TRUE,
+                                                selection = load.occ$allocation_fold,
+                                                iteration = 100, # find evenly dispersed folds
+                                                biomod2Format = FALSE,
+                                                xOffset = 0, # shift the blocks horizontally
+                                                yOffset = 0)))
+     if(inherits(a, 'try-error'))
+     {
+       output$Envbug_sp <- renderUI(p('Spatial blocking failed, please check your inputs and try again!'))
+     } else {
+       output$Envbug_sp <- renderUI(p())
+       load.occ$spatialblock<-a
+       a
+     }
+   })
 
   output$sp_block<-renderPlot({
     spatialblock<-spatialblock()
@@ -57,9 +62,9 @@ output$ui_spatial_blocks<-renderUI({
              summarise_fold(spatialblock)))
       if(inherits(a, 'try-error'))
       {
-        output$Envbug_sp <- renderUI(p('Spatial blocking failed, please check your inputs and try again!'))
+        output$Envbug_fold <- renderUI(p('Spatial blocking failed, please check your inputs and try again!'))
       } else {
-        output$Envbug_sp <- renderUI(p())
+        output$Envbug_fold <- renderUI(p())
         a
       }
     })
@@ -85,13 +90,13 @@ output$ui_spatial_blocks<-renderUI({
                                                      sliderInput("number_fold", "folds", min=1, max=100, value=5),
                                                      selectInput("allocation_fold","allocation of blocks to folds",choices = c("random","systematic"),selected="random"),
                                                      sliderInput("test_fold","Select the number of fold to assign as test dataset",min = 1,max=100,value = 1),
-                                                     myActionButton("sp_block",label=("Apply"), "primary"),
+                                                     myActionButton("block_button",label=("Apply"), "primary"),
                                                      uiOutput("Envbug_sp"),
                                                      plotOutput("sp_block"),
                                                      plotOutput("test_train_plot")),
                                             tabPanel("Fold summary",
                                                      p('The percentage values indicate the percentage of data the test dataset corresponds to'),
-                                                     uiOutput("Envbug_sp"),
+                                                     uiOutput("Envbug_fold"),
                                                      DT::dataTableOutput("sum_fold"))
 
            ),
